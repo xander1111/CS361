5e28fa0c05187f02b951a175dce13a0c049aff04
Xander Bailey <xander.bailey@gmail.com>
2023-03-05 00:38:01 -0800

    Make items fall instead of despawning

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -2,25 +2,24 @@ using UnityEngine;
 
 public abstract class Item : MonoBehaviour
 {
-    private float _timeBeforeDespawn;
-    private float _timeSpawned;
+    private const float FallSpeed = 2f;
     
     protected abstract void Collect();
 
-    private void OnEnable()
-    {
-        _timeBeforeDespawn = 10f;
-        _timeSpawned = Time.time;
-    }
-
     private void FixedUpdate()
     {
-        if (Time.time - _timeSpawned > _timeBeforeDespawn) Destroy(gameObject);
+        transform.position += new Vector3(0, -1 * FallSpeed, 0) * Time.deltaTime;
+
+        float minVisibleY = transform.position.y - transform.lossyScale.y / 2;
+        if (minVisibleY < GameManager.minVisibleY) Destroy(gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
-        Debug.Log("Superclass Collided");
-        if (other.transform.CompareTag("BedCollider")) Collect();
+        if (other.transform.CompareTag("BedCollider"))
+        {
+            Collect();
+            Destroy(gameObject);
+        }
     }
 }
