eee9e9f07c115db758cec94e7ff429666d7a023b
Xander Bailey <xander.bailey@gmail.com>
2023-03-05 00:38:39 -0800

    Randomize item spawn locations

--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class ItemManager : MonoBehaviour
 {
-    private static float _itemAppearTimeMin = 30f;
-    private static float _itemAppearTimeMax = 45f;
+    private static float _itemAppearTimeMin = 60f;
+    private static float _itemAppearTimeMax = 90f;
     private static System.Random _random;
 
     public List<Item> items;
@@ -23,15 +23,16 @@ public class ItemManager : MonoBehaviour
         {
             // Use a random permutation of all available items to prevent items
             // from not appearing for an extended period of time
-            Item[] itemOrder = items.ToArray();
-            itemOrder = itemOrder.OrderBy(x => _random.Next()).ToArray();
+            Item[] itemOrder = items.OrderBy(x => _random.Next()).ToArray(); 
 
             foreach (Item item in itemOrder)
             {
                 float waitTime = Random.Range(_itemAppearTimeMin, _itemAppearTimeMax);
                 yield return new WaitForSeconds(waitTime);
+                
+                Vector2 spawnLocation = new Vector2(Random.Range(GameManager.minVisibleX, GameManager.maxVisibleX), 1);
 
-                Instantiate(item, new Vector3(0, Bed.maxY, 0), new Quaternion());
+                Instantiate(item, spawnLocation, new Quaternion());
             }
         }
     }
--- Assets/Scripts/ItemSpeedDown.cs
+++ Assets/Scripts/ItemSpeedDown.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class ItemSpeedDown : Item {
     protected override void Collect()
     {
-        Debug.Log("Collected");
         Monkey[] monkeys = FindObjectsOfType<Monkey>();
         foreach (Monkey monkey in monkeys) monkey.DecreaseSpeed();
     }
