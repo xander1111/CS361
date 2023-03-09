f44550602d7c7744ce36a4f56b367b22a8e2b76d
Xander Bailey <xander.bailey@gmail.com>
2023-03-09 14:09:01 -0800

    Fix items not moving downward

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -5,14 +5,14 @@ public abstract class Item : MonoBehaviour
     private const float FallSpeed = 2f;
     
     protected abstract void Collect();
-
+    
     private void FixedUpdate()
     {
         Transform tsf = transform;
-        Vector3 pos = tsf.position;
-        pos += new Vector3(0, -1 * FallSpeed, 0) * Time.deltaTime;
-
-        float minVisibleY = pos.y - tsf.lossyScale.y / 2;
+        tsf.position += new Vector3(0, -1 * FallSpeed, 0) * Time.deltaTime;
+        
+        // ReSharper disable once Unity.InefficientPropertyAccess
+        float minVisibleY = tsf.position.y - tsf.lossyScale.y / 2;
         if (minVisibleY < GameManager.minVisibleY) Destroy(gameObject);
     }
 
